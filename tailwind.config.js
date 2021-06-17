60c3c630b333c79418e08afc45cbd03fbc4ae19f
paul <tikitokenfinance@gmail.com>
2021-06-17 15:11:19 -0400

    temporary presale site

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = windmill({
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        sans: ['Orbitron', ...defaultTheme.fontFamily.sans],
       },
       boxShadow: {
         bottom: '0 5px 6px -7px rgba(0, 0, 0, 0.6), 0 2px 4px -5px rgba(0, 0, 0, 0.06)',
